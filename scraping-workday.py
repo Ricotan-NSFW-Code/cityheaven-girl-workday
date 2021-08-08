87d06e4f2188bd7b9f2c397e37b8757a5c2bd398
Rico Shiraishi <shun812linus@gmail.com>
2021-08-08 18:34:00 +0900

    first commit

---
+++ scraping-workday.py
@@ -0,0 +1,47 @@
+from bs4 import BeautifulSoup
+import re
+
+plist = []
+alist = []
+channel = client.get_channel(message.channel)
+await message.delete()
+target_url = ""####Please Input Your Like Girl
+r = requests.get(target_url)
+soup = BeautifulSoup(r.content, 'lxml')
+s = soup.find_all("div", class_="go2")
+ul = soup.find_all("dt")
+for p in s:
+    p = str(p)
+    p = de.sub("", p)
+    print()
+    p = p.replace("\n", "")
+    p = p.replace(" ", "")
+    plist.append(p)
+for a in ul[28:36]:
+    a = str(a)
+    a = a.replace("\n", "")
+    a = a.replace(" ", "")
+    alist.append(a)
+for i in range(len(plist)):
+    b = alist.pop(0)
+    c = plist.pop(0)
+    b = re.sub(r'[a-z]', "", b)
+    b = re.sub('\n', "", b)
+    b = b.replace("</dt>", "")
+    b = b.replace("<", "")
+    b = b.replace(">", "")
+    b = b.replace("\"", "")
+    b = b.replace("=", "")
+    b = b.replace(" ", "")
+    b = b.rstrip("/")
+    c = re.sub(r'[a-z]', "", c)
+    c = re.sub('\n', "", c)
+    c = c.replace("</dt>", "")
+    c = c.replace("<", "")
+    c = c.replace(">", "")
+    c = c.replace("\"", "")
+    c = c.replace("=", "")
+    c = c.replace(" ", "")
+    o = c + b
+    o = o.replace(" ", "")
+    await message.channel.send(o)
